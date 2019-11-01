14ed02e987edc48e84d70fb7868a5155e6600f04
cjogles <jackson.ogles28@gmail.com>
2019-10-31 18:33:58 -0600

    finished tests for smoke testing

--- baseball-stats-keeper/src/App.test.js
+++ baseball-stats-keeper/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
+import { testNameToKey } from 'jest-snapshot/build/utils';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Display renders to screen without crashing", () => {
+  render(<Display/>);
+});
+
+test("Dashboard renders to screen without crashing", () => {
+  render(<Dashboard/>);
 });
