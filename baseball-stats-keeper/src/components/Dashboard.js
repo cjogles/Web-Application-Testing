2c4dfb13ddb789fe94abadaa46e206e82f341a26
cjogles <jackson.ogles28@gmail.com>
2019-10-31 18:20:29 -0600

    completed strike, ball, foul, and hit functionality

--- baseball-stats-keeper/src/components/Dashboard.js
+++ baseball-stats-keeper/src/components/Dashboard.js
@@ -1,10 +1,58 @@
-// Dashboard Component
-
-import React from 'react';
+import React, {useState} from 'react';
+import Display from './Display';
 
 const Dashboard = () => {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [fouls, setFouls] = useState(0);
+  const [hits, setHits] = useState(0);
+
+function addStrike() {
+  if (strikes > 2) {
+    setBalls(0);
+    setStrikes(0);
+  }
+  if (balls > 3) {
+    setBalls(0);
+    setStrikes(0);
+  }
+  setStrikes(strikes + 1);
+}
+
+const addBall = () => {
+  if (strikes > 2) {
+    setBalls(0);
+    setStrikes(0);
+  }
+  if (balls > 3) {
+    setBalls(0);
+    setStrikes(0);
+  }
+  setBalls(balls + 1);
+}
+
+const addFoul = () => {
+  setFouls(fouls + 1);
+  if (strikes < 2) {
+    setStrikes(strikes + 1);
+  }
+}
+
+const addHit = () => {
+  setHits(hits + 1);
+  setBalls(0);
+  setStrikes(0);
+}
+
   return (
-    
+    <div>
+      <button onClick={addStrike}>Strike</button>
+      <button onClick={addBall}>Ball</button>
+      <button onClick={addFoul}>Foul</button>
+      <button onClick={addHit}>Hit</button>
+      <Display ballsCount={balls} strikeCount={strikes}/>
+
+    </div>
   );
 }
 
--- baseball-stats-keeper/src/components/Display.js
+++ baseball-stats-keeper/src/components/Display.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Display = () => {
+const Display = (props) => {
   return (
-    
+    <div>
+        // for the at-bat,
+        // should be updated when user records activity on the Dashboard component
+        <p>This is the number of balls</p>
+        {props.ballsCount}
+        <p>This is the number of strikes</p>
+        {props.strikeCount}
+    </div>
   );
 }
 
